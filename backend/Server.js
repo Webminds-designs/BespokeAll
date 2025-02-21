7b17b26adaeb1a1b0e4c416592b8bd3365c127b5
Rumesh Siriwardhana <99265991+rumeshsmrr@users.noreply.github.com>
2025-02-21 23:09:14 +0530

    Update Server.js

--- backend/Server.js
+++ backend/Server.js
@@ -27,7 +27,8 @@ const allowedOrigins = [
   // "http://localhost:5173",
   // "http://localhost:3001",
   "https://artisanbespokefurniture.com",
-  "https://www.artisanbespokefurniture.com/"
+  "https://www.artisanbespokefurniture.com/",
+  "https://www.artisanbespokefurniture.com"
 ];
 
 app.use(
